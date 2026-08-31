2fd7e21b099bdf38791d122ed212f2e128e2e18e
intelektzasandy251-coder <intelektzasandy251@gmail.com>
2026-08-31 05:30:14 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyCHwZ3xqprR_eDwlYmGLA5VSKYqAwIgHW4",
+  authDomain: "ustaz-ai-32848.firebaseapp.com",
+  projectId: "ustaz-ai-32848",
+  storageBucket: "ustaz-ai-32848.firebasestorage.app",
+  messagingSenderId: "650635015603",
+  appId: "1:650635015603:web:3143a4784a508d4bb587c6"
+};
+
+window.USTAZ_AI_OWNER_EMAIL = "kalibekovna21@gmail.com";
