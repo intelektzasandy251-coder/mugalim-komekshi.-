50547ede379fa3a40b98441d22cd7d2b6e148a67
intelektzasandy251-coder <intelektzasandy251@gmail.com>
2026-08-31 15:13:33 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,13 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyCHwZ3xqprR_eDwlYmGLA5VSKYqAwIgHW4",
+  authDomain: "ustaz-ai-32848.firebaseapp.com",
+  projectId: "ustaz-ai-32848",
+  storageBucket: "ustaz-ai-32848.firebasestorage.app",
+  messagingSenderId: "650635015603",
+  appId: "1:650635015603:web:3143a4784a508d4bb587c6"
+};
+
+window.USTAZ_AI_OWNER_EMAIL = "kalibekovna21@gmail.com";
+
+/* AI App Check кілтін кейін енгіземіз */
 window.USTAZ_AI_APPCHECK_SITE_KEY = "";
